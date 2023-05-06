165544499b3ad16a69db7fc7ac17e0adf79da14b
Srinjoy Das <119725096+SRINJOY59@users.noreply.github.com>
2023-05-06 20:04:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var typingEffect = new Typed(".multiText",{
-    strings: ["Student","Developer","Coder"],
-    loop: true,
-    typrSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1500
+	strings: ["Student","Developer","Coder"],
+	loop: true,
+	typrSpeed: 100,
+	backSpeed: 80,
+	backDelay: 1500
 })
